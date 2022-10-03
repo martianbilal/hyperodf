113a46e032c26f5079ab40b79ddf6230b1084406
Bilal Naeem <2016bilal1990@gmail.com>
2022-10-03 13:32:30 -0400

    Initial commit for replay PTRACE module - It will implement the translation layer for syscall

---
+++ replay-ptrace/init_ptrace.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+// this main would be used for testing our ptrace implementation
+int main(int argc, char **argv){
+    int ret = 0;
+
+
+    // [TODO] create a table for recording what values should be changed and for which 
+    // system call | should also have their translated values
+
+    // initialize the variables required for setting up the ptrace 
+    // init_env_ptrace();
+    // initialize the child process to be traced AND start tracing 
+    // register intercept handler for a system call 
+    // print something when receive the corresponding system call 
+        // check return value given by the kernel 
+        // print the value given to the user by us 
+        // return the modified value by the user 
+
+
+
+    return ret;
+}
