4af6023528c978ef59024143d2f99655900eb5ba
Bilal Naeem <2016bilal1990@gmail.com>
2023-02-21 22:55:23 -0500

    Add logging for load snapshot

--- qemu-5.2.0/run.sh
+++ qemu-5.2.0/run.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
-kill -9 $(pgrep qemu)
+set +o noclobber
+# kill -9 $(pgrep qemu)
+pkill qemu
 make -j 128
-make install
+# make install
   # -append "console=ttyS0 root=/dev/vda earlyprintk=serial net.ifnames=0" \
   # -drive file=$IMAGE/stretch_backup.qcow2,id=tst,if=none \
   # -device virtio-blk-pci,scsi=off,drive=tst \
 # strace --abbrev=none -f -o simple.log 
   # -loadvm init_vm \
-/usr/local/bin/qemu-system-x86_64 \
+# -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
+#   -net nic,model=e1000 \
+  
+./build/qemu-system-x86_64 \
   -m 1G \
   -cpu host,vmx=on \
-  -kernel $KERNEL \
+  -kernel $KERNEL/arch/x86/boot/bzImage \
   -append "console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0" \
-  -drive file=$IMAGE/stretch_backup.qcow2,format=qcow2 \
-  -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
-  -net nic,model=e1000 \
+  -drive file=$TINYIMAGE/stretch_backup.qcow2,format=qcow2 \
   -enable-kvm \
   -nographic \
   -s \
