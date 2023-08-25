ccde33763ccbb59665f93953d4bf45e3c4b4db83
Bilal <naeem3@purdue.edu>
2023-08-25 00:28:28 +0000

    This file

---
+++ qemu-5.2.0/test-qmp.py
@@ -0,0 +1,55 @@
+import socket
+import json
+
+class QemuQmpTest:
+    def __init__(self, socket_path):
+        self.socket_path = socket_path
+        self.sock = None
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.connect(self.socket_path)
+        # Read the greeting message
+        greeting = self._recv()
+        # stringify the greeting for printing
+        greeting = json.dumps(greeting, indent=4)
+        print(greeting)
+        assert 'QMP' in greeting and 'version' in greeting, "Invalid QMP greeting"
+
+    def close(self):
+        if self.sock:
+            self.sock.close()
+
+    def _send(self, cmd):
+        self.sock.sendall(json.dumps(cmd).encode())
+
+    def _recv(self):
+        response = self.sock.recv(4096).decode()
+        return json.loads(response)
+
+    def test_query_version(self):
+        self._send({"execute": "qmp_capabilities"})
+        response = self._recv()
+        assert 'return' in response, "Failed to set QMP capabilities"
+
+        self._send({"execute": "query-version"})
+        version_info = self._recv()
+        print(version_info)
+        assert 'return' in version_info, "Failed to get QEMU version"
+        assert 'qemu' in version_info['return'], "Version info does not contain QEMU details"
+
+        print(f"QEMU Version: {version_info['return']['qemu']}")
+
+        return True
+
+if __name__ == "__main__":
+    test = QemuQmpTest("./qmp.sock")
+    test.connect()
+
+    try:
+        if test.test_query_version():
+            print("Test passed!")
+        else:
+            print("Test failed!")
+    finally:
+        test.close()
