be4ee6dfd9eab5e8066bcd0ef6770b58468ac3cb
Bilal <naeem3@purdue.edu>
2023-08-27 23:22:31 +0000

    Modify testing script

--- qemu-5.2.0/test-qmp.py
+++ qemu-5.2.0/test-qmp.py
@@ -2,23 +2,6 @@ import socket
 import json
 import sys
 
-def start_child_conn(sock_path):
-    try:
-        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        sock.connect(sock_path)
-        sock.sendall(json.dumps({"execute": "qmp_capabilities"}).encode())
-        response = sock.recv(4096).decode()
-        response = json.loads(response)
-        assert 'return' in response, "Failed to set QMP capabilities"
-        sock.sendall(json.dumps({"execute": "get_hello"}).encode())
-        response = sock.recv(4096).decode()
-        response = json.loads(response)
-        assert 'return' in response, "Failed to set QMP capabilities"
-        sock.close()
-        return True
-    except Exception as e:
-        return False
-
 class QemuQmpTest:
     def __init__(self, socket_path):
         self.socket_path = socket_path
@@ -45,90 +28,70 @@ class QemuQmpTest:
         response = self.sock.recv(4096).decode()
         return json.loads(response)
 
-    def test_query_version(self):
+    def set_qmp_capabilities(self):
         self._send({"execute": "qmp_capabilities"})
         response = self._recv()
         assert 'return' in response, "Failed to set QMP capabilities"
 
+    def do_query_version(self):
         self._send({"execute": "query-version"})
         version_info = self._recv()
         print(version_info)
         assert 'return' in version_info, "Failed to get QEMU version"
         assert 'qemu' in version_info['return'], "Version info does not contain QEMU details"
 
-        print(f"QEMU Version: {version_info['return']['qemu']}")
-
-        return True
-    def test_get_hello(self):
-        self._send({"execute": "qmp_capabilities"})
-        response = self._recv()
-        assert 'return' in response, "Failed to set QMP capabilities"
-
+    def do_get_hello(self):
         self._send({"execute": "get_hello"})
         result = self._recv()
         result = json.dumps(result, indent=4)
         print(result)
         assert 'Hello' in result, "Did not get hello"
 
-        return True
-    
-    def test_vm_fork(self):
-        self._send({"execute": "qmp_capabilities"})
-        response = self._recv()
-        assert 'return' in response, "Failed to set QMP capabilities"
-        
+    def do_vm_fork(self):
         self._send({"execute": "vm_fork"})
         result = self._recv()
-        ret = False
-        # while not ret:
-            # ret = start_child_conn("child-qmp.sock")
-        
         result = json.dumps(result, indent=4)
         print(result)
-        # assert 'childpid' in result, "Did not get vm_fork"
-    
-    def test_query_iothreads(self):
-        self._send({"execute": "qmp_capabilities"})
-        response = self._recv()
-        assert 'return' in response, "Failed to set QMP capabilities"
 
+    def do_query_iothreads(self):
         self._send({"execute": "query-iothreads"})
         result = self._recv()
         result = json.dumps(result, indent=4)
         print(result)
-        assert 'return' in result, "Did not get query-iothreads"
 
-        return True
-
+def run_cmd(sock_con, cmd):
+    if cmd == "quit" or cmd == "q":
+        exit(0)
+    elif cmd == "query_version":
+        sock_con.do_query_version()
+    elif cmd == "get_hello":
+        sock_con.do_get_hello()
+    elif cmd == "query_iothreads":
+        sock_con.do_query_iothreads()
+    elif cmd == "vm_fork":    
+        sock_con.do_vm_fork()
+    else:
+        print(f"Unknown command: {cmd}")
 
 if __name__ == "__main__":
     # take the arg from cmdline to pass the socket path
     sock_path = sys.argv[1]
-    cmd = sys.argv[2]
-    
-    test = QemuQmpTest(sock_path)
-    test.connect()
-
     try:
-        if cmd == "query-version":
-            if test.test_query_version():
-                print("Test passed!")
-            else:
-                print("Test failed!")
-        elif cmd == "get_hello":
-            if test.test_get_hello():
-                print("Test passed!")
-            else:
-                print("Test failed!")
-        elif cmd == "query_iothreads":
-            if test.test_query_iothreads():
-                print("Test passed!")
-            else:
-                print("Test failed!")
-        elif cmd == "vm_fork":    
-            if test.test_vm_fork():
-                print("Test passed!")
-            else:
-                print("Test failed!")
+        cmd = sys.argv[2]
+    except:
+        cmd = None
+        
+    
+    sock_con = QemuQmpTest(sock_path)
+    sock_con.connect()
+    sock_con.set_qmp_capabilities()
+    if cmd :
+        run_cmd(sock_con, cmd)
+        exit(0)
+    
+    try:
+        while True:
+            cmd = input("Enter command (or 'quit' to exit): ")
+            run_cmd(sock_con, cmd)
     finally:
-        test.close()
+        sock_con.close()
