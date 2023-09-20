b5d06d6e53b2669978d272ad52f07c8d243c705c
Bilal Naeem <2016bilal1990@gmail.com>
2023-09-20 16:40:48 -0400

    Port changes to channel-socket.c

--- qemu-5.2.0/io/channel-socket.c
+++ qemu-5.2.0/io/channel-socket.c
@@ -26,6 +26,7 @@
 #include "io/channel-watch.h"
 #include "trace.h"
 #include "qapi/clone-visitor.h"
+#include "util/hodf-util.h"
 
 #define SOCKET_MAX_FDS 16
 
@@ -107,6 +108,9 @@ qio_channel_socket_set_fd(QIOChannelSocket *sioc,
     if (sioc->localAddr.ss_family == AF_UNIX) {
         QIOChannel *ioc = QIO_CHANNEL(sioc);
         qio_channel_set_feature(ioc, QIO_CHANNEL_FEATURE_FD_PASS);
+        // TODO[Bilal]
+        // still hacky but better than earlier hardcoded value
+        h_set_qmp_server_fd(fd);
     }
 #endif /* WIN32 */
 
@@ -558,6 +562,8 @@ static ssize_t qio_channel_socket_writev(QIOChannel *ioc,
     }
 
  retry:
+    // register with hodf utility
+    h_register_monitor_fd(sioc->fd);
     ret = sendmsg(sioc->fd, &msg, 0);
     if (ret <= 0) {
         if (errno == EAGAIN) {
