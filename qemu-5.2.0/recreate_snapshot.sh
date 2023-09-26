fbabb302553d6aee37b3e13de73ab80559bcda68
Bilal Naeem <2016bilal1990@gmail.com>
2023-09-26 17:48:57 -0400

    Use this qemu's img utility instead of the default one

--- qemu-5.2.0/recreate_snapshot.sh
+++ qemu-5.2.0/recreate_snapshot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd $IMAGE;
-qemu-img create -f qcow2 -o backing_file=stretch.img stretch_backup.qcow2;
+$MOD_QEMU/build/qemu-img create -f qcow2 -o backing_file=stretch.img stretch_backup.qcow2;
 cd ~/kvm-samples/qemu-5.2.0;
